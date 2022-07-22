d5ee27679d720c417885420b2106a8f009400bcd
Zeus Security <Redsec1337@protonmail.com>
2022-07-22 05:41:55 -0400

    update
    update commit

--- Changelog.md
+++ Changelog.md
@@ -17,7 +17,7 @@
 ### 16. Added new vampire theme menu.
 ### 17. Removed all crash options.
 ### 18. renamed alot of options.
-### 19. More Coming Soon!.
+### 19. More Coming Soon!
 ###
 ### It now shows deatiled information about the player upon entering proximity.
 ### When a player enters the proximity it will take weapons away temporarily.
--- Dracula.lua
+++ Dracula.lua
@@ -4,7 +4,7 @@ if Dracula_version then
 end 
 
 --Set Version Here requeriment for the script to work
-Dracula_version = "20.978"
+Dracula_version = "20.979"
 
 menu.create_thread(function()
 
@@ -6812,6 +6812,8 @@ SUB_CL_ = "Cooldown Removed\nMissiles Range Improved"
 ---------------------------------------------------------------------------------------------------------------------------------
 --Outift
 Outfit = menu.add_feature("Outfitter", "parent", recovery.id)
+Headwear = menu.add_feature("Headwear", "parent", Outfit.id)
+
 
 
 local money =
